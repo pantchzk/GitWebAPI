d2a66b66b63b74295dc1d8077feffdaaed065687
pantchzk <pantchzk@mail.uc.edu>
2023-10-30 19:06:54 -0400

    Updating controller

---
+++ GitWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    
+    public class HomeController : Controller
+    {
+        [HttpGet(Name = "GetStudentName")]
+        public IActionResult get()
+        {
+            return Json( new { Name="Zackary Pantchev" });
+        }
+    }
+}
